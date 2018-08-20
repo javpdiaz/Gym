3333d74c4ac7da2c7c20f6cd89dc165f02679a9e
Javier Peralta Diaz <javpdiaz@gmail.com>
2018-08-20 10:42:52 -0500

    Primer commit

---
+++ src/AppBundle/Controller/PrincipalController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PrincipalController extends Controller
+{
+    /**
+     * @Route("/", name="principal_page")
+     */
+    public function PrincipalPage(Request $request)
+    {
+        return $this->render('principal/principal.twig', array('activeconf'=>true));
+    }
+
+    /**
+     * @Route("/galeria", name="galeria_page")
+     */
+    public function GaleriaPage(){
+        return $this->render('principal/galeria.html.twig', array('activeconf'=>true));
+    }
+
+    /**
+     * @Route("/entrenamiento", name="entremaniento_page")
+     */
+    public function EntrenamientoPage(){
+        return $this->render('principal/entrenamiento.twig');
+    }
+
+    /**
+     * @Route("/precios", name="precios_page")
+     */
+    public function PreciosPage(){
+        return $this->render('principal/precios.html.twig');
+    }
+
+    /**
+     * @Route("/blog", name="blog_name")
+     */
+    public function BlogPage(){
+        return $this->render('principal/blog.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contacto_page")
+     */
+    public function ContactoPage(){
+        return $this->render('principal/contact.html.twig');
+    }
+}
---
+++ src/AppBundle/Entity/Contacto.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contacto
+ *
+ * @ORM\Table(name="contacto")
+ * @ORM\Entity
+ */
+class Contacto
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255, nullable=false)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apellido", type="string", length=255, nullable=false)
+     */
+    private $apellido;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, nullable=false)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="asunto", type="string", length=255, nullable=false)
+     */
+    private $asunto;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mensaje", type="string", length=255, nullable=false)
+     */
+    private $mensaje;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id_contacto", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idContacto;
+
+
+}
+
